4801f30dd69605466395af3270f1721c35f07862
ShuaPetey <joshua.peterson3@cuw.edu>
2023-03-29 13:15:17 -0500

    Added true/false statements to turn on/off exits

---
+++ Dungeon Crawler/Assets/Scripts/dungeonSetup.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dungeonSetup : MonoBehaviour
+{
+    public GameObject northExit;
+    public GameObject southExit;
+    public GameObject eastExit;
+    public GameObject westExit; 
+
+    public bool northOn, southOn, eastOn, westOn; // can define multiple of same type this way
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        if(northOn == false)
+        {
+            this.northExit.SetActive(false);
+        }
+        if(southOn == false)
+        {
+            this.southExit.SetActive(false);
+        }
+        if(eastOn == false)
+        {
+            this.eastExit.SetActive(false);
+        }
+        if(westOn == false)
+        {
+            this.westExit.SetActive(false);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
