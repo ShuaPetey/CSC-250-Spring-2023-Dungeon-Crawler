c853cff9ce33b49b9572369859624415205cb6cd
ShuaPetey <joshua.peterson3@cuw.edu>
2023-04-03 11:27:00 -0500

    Update PlayerController.cs

--- Dungeon Crawler/Assets/PlayerController.cs
+++ Dungeon Crawler/Assets/PlayerController.cs
@@ -9,38 +9,44 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
     public float movementSpeed = 40.0f;
+    private bool isMoving;
 
     // Start is called before the first frame update
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
-        print(MasterData.count);
+        //print(MasterData.count);
         MasterData md = new MasterData();
+        this.isMoving = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.UpArrow))
+        if(Input.GetKeyDown(KeyCode.UpArrow) && this.isMoving == false)
         {
-            this.rb.transform.position = new Vector3(0f, 0f, 0f);
+            //this.rb.transform.position = new Vector3(0f, 0f, 0f);
             this.rb.AddForce(this.northExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if(Input.GetKeyDown(KeyCode.DownArrow))
+        if(Input.GetKeyDown(KeyCode.DownArrow) && this.isMoving == false)
         {
-            this.rb.transform.position = new Vector3(0f, 0f, 0f);
+            //this.rb.transform.position = new Vector3(0f, 0f, 0f);
             this.rb.AddForce(this.southExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && this.isMoving == false)
         {
-            this.rb.transform.position = new Vector3(0f, 0f, 0f);
+            //this.rb.transform.position = new Vector3(0f, 0f, 0f);
             this.rb.AddForce(this.westExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
-        if(Input.GetKeyDown(KeyCode.RightArrow))
+        if(Input.GetKeyDown(KeyCode.RightArrow) && this.isMoving == false)
         {
-            this.rb.transform.position = new Vector3(0f, 0f, 0f);
+            //this.rb.transform.position = new Vector3(0f, 0f, 0f);
             this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
+            this.isMoving = true;
         }
         
     }
