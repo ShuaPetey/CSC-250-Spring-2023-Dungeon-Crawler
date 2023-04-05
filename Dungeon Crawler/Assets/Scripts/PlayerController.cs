1c2c5de7bb8f6baf2a232ee15ac551c111b7f063
ShuaPetey <joshua.peterson3@cuw.edu>
2023-04-05 14:39:27 -0500

    Got ball to roll out of exit and come to stopping point

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public GameObject northExit, southExit, eastExit, westExit;
+    public GameObject westStart, eastStart, northStart, southStart;
     public float movementSpeed = 40.0f;
     private bool isMoving;
 
@@ -15,10 +16,33 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         this.rb = this.GetComponent<Rigidbody>();
-        //print(MasterData.count);
-        MasterData md = new MasterData();
         this.isMoving = false;
-        print(MasterData.whereDidIComeFrom);
+
+        if(!MasterData.whereDidIComeFrom.Equals("?"))
+        {
+            if(MasterData.whereDidIComeFrom.Equals("North"))
+            {
+                this.gameObject.transform.position = this.southExit.transform.position;
+                this.rb.AddForce(Vector3.back * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("south"))
+            {
+                this.gameObject.transform.position = this.northExit.transform.position;
+                this.rb.AddForce(Vector3.forward * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("east"))
+            {
+                this.gameObject.transform.position = this.westExit.transform.position;
+                this.rb.AddForce(Vector3.left * 150.0f);
+            }
+            else if(MasterData.whereDidIComeFrom.Equals("west"))
+            {
+                this.gameObject.transform.position = this.eastExit.transform.position;
+                this.rb.AddForce(Vector3.right * 150.0f);
+            }
+        }
+        
+        
 
     }
 
@@ -51,26 +75,35 @@ public class PlayerController : MonoBehaviour
         }
         
     }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag.Equals("Center"))
+        {
+            this.rb.velocity = Vector3.zero;
+            this.rb.angularVelocity = Vector3.zero;
+        }
+    }
+
     private void OnTriggerExit(Collider other)
         {
-            if(other.gameObject.tag.Equals("Exit"))
+            if(other.gameObject.tag.Equals("Exit") && MasterData.isExiting)
             {
                 if(other.gameObject == this.northExit)
                 {
                     MasterData.whereDidIComeFrom = "North";
                     MasterData.count++;
-                    SceneManager.LoadScene("DungeonRoom");
-                    this.rb.transform.position = new Vector3(0f, .5f, 4.5f);
-                    this.rb.AddForce(this.northExit.transform.position * movementSpeed);
+                    //SceneManager.LoadScene("DungeonRoom");
+                    //this.rb.transform.position = new Vector3(0f, .5f, 4.5f);
+                    //this.rb.AddForce(this.northExit.transform.position * movementSpeed);
                     
                 }
                 else if(other.gameObject == this.southExit)
                 {
                     MasterData.whereDidIComeFrom = "south";
                     MasterData.count++;
-                    SceneManager.LoadScene("DungeonRoom");
-                    this.rb.transform.position = new Vector3(0f, .5f, -4.5f);
-                    this.rb.AddForce(this.southExit.transform.position * movementSpeed);
+                    //SceneManager.LoadScene("DungeonRoom");
+                    //this.rb.transform.position = new Vector3(0f, .5f, -4.5f);
+                    //this.rb.AddForce(this.southExit.transform.position * movementSpeed);
                     
                     
                 }
@@ -78,21 +111,26 @@ public class PlayerController : MonoBehaviour
                 {
                     MasterData.whereDidIComeFrom = "east";
                     MasterData.count++;
-                    SceneManager.LoadScene("DungeonRoom");
-                    this.rb.transform.position = new Vector3(4.5f, .5f, 0f);
-                    this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
+                    //SceneManager.LoadScene("DungeonRoom");
+                    //this.rb.transform.position = new Vector3(4.5f, .5f, 0f);
+                    //this.rb.AddForce(this.eastExit.transform.position * movementSpeed);
                     
                 }
                 else if(other.gameObject == this.westExit)
                 {
                     MasterData.whereDidIComeFrom = "west";
                     MasterData.count++;
-                    SceneManager.LoadScene("DungeonRoom");
-                    this.rb.transform.position = new Vector3(-4.5f, .5f, 0f);
-                    this.rb.AddForce(this.westExit.transform.position * movementSpeed);
+                    //SceneManager.LoadScene("DungeonRoom");
+                    //this.rb.transform.position = new Vector3(-4.5f, .5f, 0f);
+                    //this.rb.AddForce(this.westExit.transform.position * movementSpeed);
                     
                 }
-                
+                MasterData.isExiting = false;
+                SceneManager.LoadScene("DungeonRoom");
+            }
+            else if(other.gameObject.tag.Equals("Exit") && !MasterData.isExiting)
+            {
+                MasterData.isExiting = true;
             }
         }
 }
