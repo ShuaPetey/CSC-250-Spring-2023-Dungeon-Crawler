6d482bd899e453a15abda2918ff1d7933afcf984
ShuaPetey <joshua.peterson3@cuw.edu>
2023-04-13 22:18:17 -0500

    Update PlayerController.cs

--- Dungeon Crawler/Assets/Scripts/PlayerController.cs
+++ Dungeon Crawler/Assets/Scripts/PlayerController.cs
@@ -55,11 +55,11 @@ public class PlayerController : MonoBehaviour
             //this.rb.transform.position = new Vector3(0f, 0f, 0f);
             this.rb.AddForce(this.northExit.transform.position * movementSpeed);
             this.isMoving = true;
-            /*if(northOn == false)
+            if(northOn == false)
             {
                 this.rb.AddForce(this.northExit.transform.position * 0.0f);
             }
-            */
+        
         }
         if(Input.GetKeyDown(KeyCode.DownArrow) && this.isMoving == false)
         {
